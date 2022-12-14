2ac7a9cc6f0796e4c9edd56130d8b3823cd8e77e
Sena0820 <fujingho747@gmail.com>
2022-12-14 22:56:34 +0900

    add search_hill_climb.py

--- search_hill_climb.py
+++ search_hill_climb.py
@@ -3,12 +3,19 @@ from abc import ABC
 import numpy as np
 import random
 from simpleai.search import SearchProblem
-from simpleai.search.local import hill_climbing, simulated_annealing
+from simpleai.search.local import hill_climbing_stochastic, simulated_annealing
 from simpleai.search.viewers import ConsoleViewer
-# from gensim.models import KeyedVectors
+from gensim.models import KeyedVectors
 from gensim.models import word2vec
 import requests
 from bs4 import BeautifulSoup
+
+NUM_CANDIDATES = 5  # 一度に探索する単語の数
+
+vectors = word2vec.Word2Vec.load("word2vec.gensim.model")
+
+# target = "初心者"
+# target_vector = vectors[target]
 
 
 def return_rank(query):
@@ -33,7 +40,7 @@ def return_rank(query):
     soup = BeautifulSoup(request.text, "html.parser")
     search_site_list = soup.select('div.kCrYT > a')
 
-    num = random.randint(1, 10)
+    num = random.randint(1, 30)
     rank1 = 1 / (50 + num)
 
     # ページ解析と結果の出力
@@ -57,25 +64,15 @@ def return_rank(query):
     return rank1
 
 
-NUM_CANDIDATES = 2  # 一度に探索する単語の数
-
-vectors = model = word2vec.Word2Vec.load("word2vec.gensim.model")
-
-
-# target = "作成"
-# target_vector = vectors[target]
-
-
-class QuerySearchProblem(SearchProblem, ABC):
+class QuerySearchProblem(SearchProblem):
     # 次の状態に至るための action のリストを生成
     # ここでは次のクエリ候補を生成している
     def actions(self, curr_query):
-        actions = []
-        curr_vector = vectors.wv[curr_query]
+        actions = [curr_query]
+        curr_vector = vectors[curr_query]
         for _ in range(NUM_CANDIDATES):
-            new_vector = curr_vector + (np.random.standard_normal(curr_vector.size))*0.2
-            # new_vector = curr_vector + 0.3,これだと次のベクトルが変わらない可能性がある
-            new_keywords = vectors.wv.similar_by_vector(new_vector)
+            new_vector = curr_vector + np.random.uniform(size=curr_vector.size, low=-0.5, high=0.5)
+            new_keywords = vectors.similar_by_vector(new_vector, topn=NUM_CANDIDATES)
             for k, _ in new_keywords:
                 # 現在のqueryと異なり，かつすでにクエリ候補に入っていない単語をクエリ候補とする
                 if k != curr_query and k not in actions:
@@ -94,15 +91,17 @@ class QuerySearchProblem(SearchProblem, ABC):
         # curr_vector = vectors[curr_query]
         # d = curr_vector - target_vector
         # v = 1.0 / (1.0 + np.linalg.norm(d))
-        # print(f"query = {curr_query}, value={v}")
         v = return_rank(curr_query)
-        print(f"検索ワード：Python 初心者 {curr_query},検索順位={1 / v}")
+        print(f"query = {curr_query}, 順位={1/v}")
         return v
 
 
-initial_query = "徹底"
+initial_query = "説明"
 problem = QuerySearchProblem(initial_state=initial_query)
-result = simulated_annealing(problem, iterations_limit=10, viewer=ConsoleViewer())
-# result = hill_climbing(problem, iterations_limit=10, viewer=None)
+# result = simulated_annealing(problem, iterations_limit=100, viewer=ConsoleViewer())
+result = hill_climbing_stochastic(problem, iterations_limit=10, viewer=ConsoleViewer())
 
 print(result.path())
+# a = vectors.wv['Python']
+# print(a)
+# print(vectors.similar_by_vector(a,topn = 5))
--- test_simulated_annealing.py
+++ test_simulated_annealing.py
@@ -1,7 +1,7 @@
 import numpy as np
 import random
 from simpleai.search import SearchProblem
-from simpleai.search.local import hill_climbing, simulated_annealing
+from simpleai.search.local import hill_climbing_stochastic, simulated_annealing
 from simpleai.search.viewers import ConsoleViewer
 from gensim.models import KeyedVectors
 from gensim.models import word2vec
@@ -9,7 +9,7 @@ import requests
 from bs4 import BeautifulSoup
 
 
-NUM_CANDIDATES = 3  # 一度に探索する単語の数
+NUM_CANDIDATES = 5  # 一度に探索する単語の数
 
 vectors = word2vec.Word2Vec.load("word2vec.gensim.model")
 
@@ -17,16 +17,48 @@ target = "初心者"
 target_vector = vectors[target]
 
 
+# class QuerySearchProblem(SearchProblem):
+#     # 次の状態に至るための action のリストを生成
+#     # ここでは次のクエリ候補を生成している
+#     def actions(self, curr_query):
+#         actions = []
+#         curr_vector = vectors.wv[curr_query]
+#         for _ in range(NUM_CANDIDATES):
+#             new_vector = curr_vector + (np.random.standard_normal(curr_vector.size))* 0.2
+#             # new_vector = curr_vector + np.random.randint(0, 0.5, size=curr_vector.size)
+#             new_keywords = vectors.wv.similar_by_vector(new_vector)
+#             for k, _ in new_keywords:
+#                 # 現在のqueryと異なり，かつすでにクエリ候補に入っていない単語をクエリ候補とする
+#                 if k != curr_query and k not in actions:
+#                     actions.append(k)
+#                     break
+#         return actions
+#
+#     # 状態にアクションを適用した際の次の状態を生成
+#     # アクション＝次のクエリ候補なのでそのまま戻す
+#     def result(self, curr_query, action):
+#         return action
+#
+#     # 状態の価値を計算
+#     # ここではベクトルの差（ノルム）の逆数を価値としている
+#     def value(self, curr_query):
+#         curr_vector = vectors.wv[curr_query]
+#         d = curr_vector - target_vector
+#         v = 1.0 / (1.0 + np.linalg.norm(d))
+#         print(f"query = {curr_query}, value={v}")
+#         # v = return_rank(curr_query)
+#         # print(f"検索ワード：Python {curr_query},検索順位={v*50}")
+#         return v
+
 class QuerySearchProblem(SearchProblem):
     # 次の状態に至るための action のリストを生成
     # ここでは次のクエリ候補を生成している
     def actions(self, curr_query):
-        actions = []
-        curr_vector = vectors.wv[curr_query]
+        actions = [curr_query]
+        curr_vector = vectors[curr_query]
         for _ in range(NUM_CANDIDATES):
-            new_vector = curr_vector + (np.random.standard_normal(curr_vector.size))* 0.2
-            # new_vector = curr_vector + np.random.randint(0, 0.5, size=curr_vector.size)
-            new_keywords = vectors.wv.similar_by_vector(new_vector)
+            new_vector = curr_vector + np.random.uniform(size=curr_vector.size, low=-0.5, high=0.5)
+            new_keywords = vectors.similar_by_vector(new_vector, topn=NUM_CANDIDATES)
             for k, _ in new_keywords:
                 # 現在のqueryと異なり，かつすでにクエリ候補に入っていない単語をクエリ候補とする
                 if k != curr_query and k not in actions:
@@ -42,18 +74,19 @@ class QuerySearchProblem(SearchProblem):
     # 状態の価値を計算
     # ここではベクトルの差（ノルム）の逆数を価値としている
     def value(self, curr_query):
-        curr_vector = vectors.wv[curr_query]
+        curr_vector = vectors[curr_query]
         d = curr_vector - target_vector
         v = 1.0 / (1.0 + np.linalg.norm(d))
         print(f"query = {curr_query}, value={v}")
-        # v = return_rank(curr_query)
-        # print(f"検索ワード：Python {curr_query},検索順位={v*50}")
         return v
 
 
 initial_query = "若手"
 problem = QuerySearchProblem(initial_state=initial_query)
-result = simulated_annealing(problem, iterations_limit=10, viewer=ConsoleViewer())
-# result = hill_climbing(problem, iterations_limit=50, viewer=ConsoleViewer())
+# result = simulated_annealing(problem, iterations_limit=100, viewer=ConsoleViewer())
+result = hill_climbing_stochastic(problem, iterations_limit=50, viewer=ConsoleViewer())
 
 # print(result.path())
+# a = vectors.wv['Python']
+# print(a)
+# print(vectors.similar_by_vector(a,topn = 5))
